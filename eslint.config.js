154698e3e7c1ba618d5b26547a26781c1f6d124c
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-06 08:34:49 +0000

    fix: align dependency updates with lint and type checking

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import * as config from '@lvce-editor/eslint-config'
+import { defineConfig } from 'eslint/config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedNode,
   {
@@ -31,4 +32,4 @@ export default [
       'unicorn/prefer-top-level-await': 'off'
     }
   }
-]
+])
